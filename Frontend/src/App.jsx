fb20ce0fc6034d777ababb4f352eaf1e8ba56c41
vindeacy <deacydavids70@gmail.com>
2025-01-28 12:14:50 +0300

    google sign up done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { CartProvider } from './context/CartContext';
 import { SearchProvider } from './context/SearchContext';  // Ensure correct import
 import { WishlistProvider } from './context/WishlistContext';  // Ensure correct import
 import { Outlet } from 'react-router-dom';
+
 // import Header from './Homepage/Header';  // Import the Header component
 
 function App() {
